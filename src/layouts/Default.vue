680537ff5b54a3d6608e518a83a944ce2a3ccd3a
Travis Reynolds <travis@cirruscreative.co.uk>
2019-05-10 15:36:45 +0100

    Create default layout with sidebar content

--- src/layouts/Default.vue
+++ src/layouts/Default.vue
@@ -1,15 +1,51 @@
 <template>
-  <div class="layout">
-    <header class="header">
-      <strong>
-        <g-link to="/">{{ $static.metaData.siteName }}</g-link>
-      </strong>
-      <nav class="nav">
-        <g-link class="nav__link" to="/">Home</g-link>
-        <g-link class="nav__link" to="/about">About</g-link>
-      </nav>
-    </header>
-    <slot/>
+  <div className="container">
+    <!-- <HelmetDatoCms
+      favicon={data.datoCmsSite.faviconMetaTags}
+      seo={data.datoCmsHome.seoMetaTags}
+    /> -->
+    <div className="container__sidebar">
+      <div className="sidebar">
+        <h6 className="sidebar__title">
+          <g-link to="/">{{ $static.metaData.siteName }}</g-link>
+        </h6>
+        <div
+          className="sidebar__intro"
+          v-html="datoCmsHome.introText"
+        />
+        <ul className="sidebar__menu">
+          <li>
+            <g-link class="nav__link" to="/">Home</g-link>
+          </li>
+          <li>
+            <g-link class="nav__link" to="/about">About</g-link>
+          </li>
+        </ul>
+        <p className="sidebar__social">
+          <a
+            v-for="({ node: profile }) in $static.allDatoCmsSocialProfile.edges"
+            :key="profile.id"
+            :href="profile.url"
+            :class="`social social--${profile.profileType.toLowerCase()}`"
+            target="blank"
+          > </a>
+        </p>
+        <div className="sidebar__copyright">{{ datoCmsHome.copyright }}</div>
+      </div>
+    </div>
+    <div className="container__body">
+      <div className="container__mobile-header">
+        <div className="mobile-header">
+          <div className="mobile-header__menu">
+            <Link to="#" data-js="toggleSidebar" />
+          </div>
+          <div className="mobile-header__logo">
+            <g-link to="/">{{ $static.metaData.siteName }}</g-link>
+          </div>
+        </div>
+      </div>
+      <slot/>
+    </div>
   </div>
 </template>
 
@@ -18,5 +54,34 @@ query {
   metaData {
     siteName
   }
+  allDatoCmsHome {
+    edges {
+      node {
+        id
+        path
+        introText
+        copyright
+      }
+    }
+  }
+  allDatoCmsSocialProfile {
+    edges {
+      node {
+        id
+        profileType
+        url
+      }
+    }
+  }
 }
 </static-query>
+
+<script>
+export default {
+  computed: {
+    datoCmsHome () {
+      return this.$static.allDatoCmsHome.edges[0].node
+    }
+  }
+}
+</script>

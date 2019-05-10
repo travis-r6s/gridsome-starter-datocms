32abc0c8daf9427d289d55e2d8dcd3b654e4535e
Travis Reynolds <travis@cirruscreative.co.uk>
2019-05-10 15:40:49 +0100

    change className to class

--- src/layouts/Default.vue
+++ src/layouts/Default.vue
@@ -1,19 +1,16 @@
 <template>
-  <div className="container">
+  <div class="container">
     <!-- <HelmetDatoCms
       favicon={data.datoCmsSite.faviconMetaTags}
       seo={data.datoCmsHome.seoMetaTags}
-    /> -->
-    <div className="container__sidebar">
-      <div className="sidebar">
-        <h6 className="sidebar__title">
+    />-->
+    <div class="container__sidebar">
+      <div class="sidebar">
+        <h6 class="sidebar__title">
           <g-link to="/">{{ $static.metaData.siteName }}</g-link>
         </h6>
-        <div
-          className="sidebar__intro"
-          v-html="datoCmsHome.introText"
-        />
-        <ul className="sidebar__menu">
+        <div class="sidebar__intro" v-html="datoCmsHome.introText"/>
+        <ul class="sidebar__menu">
           <li>
             <g-link class="nav__link" to="/">Home</g-link>
           </li>
@@ -21,25 +18,25 @@
             <g-link class="nav__link" to="/about">About</g-link>
           </li>
         </ul>
-        <p className="sidebar__social">
+        <p class="sidebar__social">
           <a
             v-for="({ node: profile }) in $static.allDatoCmsSocialProfile.edges"
             :key="profile.id"
             :href="profile.url"
             :class="`social social--${profile.profileType.toLowerCase()}`"
             target="blank"
-          > </a>
+          ></a>
         </p>
-        <div className="sidebar__copyright">{{ datoCmsHome.copyright }}</div>
+        <div class="sidebar__copyright">{{ datoCmsHome.copyright }}</div>
       </div>
     </div>
-    <div className="container__body">
-      <div className="container__mobile-header">
-        <div className="mobile-header">
-          <div className="mobile-header__menu">
-            <Link to="#" data-js="toggleSidebar" />
+    <div class="container__body">
+      <div class="container__mobile-header">
+        <div class="mobile-header">
+          <div class="mobile-header__menu">
+            <Link to="#" data-js="toggleSidebar"/>
           </div>
-          <div className="mobile-header__logo">
+          <div class="mobile-header__logo">
             <g-link to="/">{{ $static.metaData.siteName }}</g-link>
           </div>
         </div>
@@ -77,11 +74,11 @@ query {
 </static-query>
 
 <script>
-export default {
-  computed: {
-    datoCmsHome () {
-      return this.$static.allDatoCmsHome.edges[0].node
+  export default {
+    computed: {
+      datoCmsHome() {
+        return this.$static.allDatoCmsHome.edges[0].node;
+      }
     }
-  }
-}
+  };
 </script>

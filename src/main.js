e9fe1ce32e19b9aa032fd198f8347649ad10673c
Travis Reynolds <travis@cirruscreative.co.uk>
2019-05-10 15:51:08 +0100

    Add vue-masonry plugin

--- src/main.js
+++ src/main.js
@@ -1,6 +1,9 @@
 // This is the main.js file. Import global CSS and scripts here.
 // The Client API can be used here. Learn more: gridsome.org/docs/client-api
 
+// Vue Plugins
+import { VueMasonryPlugin } from 'vue-masonry'
+
 // Import Layouts
 import DefaultLayout from '~/layouts/Default.vue'
 
@@ -8,6 +11,8 @@ import DefaultLayout from '~/layouts/Default.vue'
 import '~/styles/index.sass'
 
 export default function (Vue, { router, head, isClient }) {
+  // Use Masonry component
+  Vue.use(VueMasonryPlugin)
   // Set default layout as a global component
   Vue.component('Layout', DefaultLayout)
 }
